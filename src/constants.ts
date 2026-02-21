0fc631376995de4cc9dfda430e1357cea6df0062
Your Name <you@example.com>
2026-02-21 10:07:22 +0100

    tokens removed

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const CARDS: Card[] = cardsData as Card[];
 
 export const FACTIONS: Faction[] = Array.from(new Set(CARDS.map(c => c.faction))).filter(f => f !== 'neutral');
 
-export const DECK_SIZE_LIMIT = 25;
+export const DECK_SIZE_LIMIT = 26;
 export const COST_LIMIT = 65;
 export const MIN_UNIT_COUNT = 13;
 export const BRONZE_COPY_LIMIT = 2;
